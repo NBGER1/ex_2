2bd07356e538c5ac93ec0b594aa33c3af7df7d23
NBGER1 <naorberger@gmail.com>
2021-09-04 11:49:35 +0300

    UI working

---
+++ ex2/Assets/Scripts/GUI/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using DefaultNamespace;
+using Services;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        #region Editor
+
+        [SerializeField] private TextMeshProUGUI _rocketsLaunchedText;
+
+        #endregion
+
+        #region Methods
+
+        public void SetRocketsLaunchedText(string text)
+        {
+            _rocketsLaunchedText.text = text;
+        }
+
+        protected override UIManager GetInstance()
+        {
+            return this;
+        }
+
+        #endregion
+    }
+}
--- ex2/Assets/Scripts/Gameplay/GameplayCore.cs
+++ ex2/Assets/Scripts/Gameplay/GameplayCore.cs
@@ -1,32 +1,49 @@
 ﻿using Core;
+using GUI;
+using Notifications;
+using Services;
 using UnityEngine;
 
 namespace DefaultNamespace.Gameplay
 {
     public class GameplayCore : IUpdatable
     {
-        #region Editor
+        #region Fields
+
+        private int _rocketsLaunched = 0;
 
         #endregion
 
         #region Methods
 
+        private void OnRocketLaunched(EventParams e)
+        {
+            UIManager.Instance.SetRocketsLaunchedText((++_rocketsLaunched).ToString());
+        }
+
         public void StartGame()
         {
+            UIManager.Instance.SetRocketsLaunchedText(_rocketsLaunched.ToString());
+            GameplayServices.EventBus.Subscribe(GameplayEventType.RocketLaunched, OnRocketLaunched);
             GameplayElements.Instance.Cannon.Init(
                 GameplayElements.Instance.CannonParams,
                 //# Leave only 1 factory uncommented
-
-               GameplayElements.Instance.BombFactory
-                //   GameplayElements.Instance.BulletFactory
+                GameplayElements.Instance.BombFactory
+                //GameplayElements.Instance.BulletFactory
                 //GameplayElements.Instance.ProjectileFactory
             );
         }
 
-        #endregion
-
         public void Update()
         {
         }
+
+        #endregion
+
+        #region Properties
+
+        public int RocketsLaunched => _rocketsLaunched;
+
+        #endregion
     }
 }
